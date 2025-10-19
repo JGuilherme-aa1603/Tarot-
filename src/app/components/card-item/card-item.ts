d7c532548988c06a26ea8cf24bec9c83c096e706
koi10k <koi@YanAdm.localdomain>
2025-10-19 15:26:19 -0300

    feat: Adiciona estrutura inicial de componentes, páginas e serviço para o projeto Tarô.

--- src/app/components/card-item/card-item.ts
+++ src/app/components/card-item/card-item.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'; // IMPORTAÇÃO ÚNICA do core
+import { Card } from '../../data/tarot-cards';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-item',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './card-item.html',
-  styleUrl: './card-item.css'
+  styleUrl: './card-item.scss'
 })
-export class CardItem {
-
+export class CardItemComponent {
+  
+  // Define o Input que receberá os dados da carta da página Home
+  @Input({ required: true }) card!: Card;
+  
+  // Base do caminho das imagens (usado no template HTML)
+  imagePathBase = 'assets/images/tarot/';
 }
