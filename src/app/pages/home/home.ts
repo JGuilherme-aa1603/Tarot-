16e19682bf827b743dd4ce3c20673c3c9921eb47
koi10k <koi@YanAdm.localdomain>
2025-10-19 15:32:39 -0300

    Adiciona a estrutura dos componentes de página (Home, Cartas, Tiragem)

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardService } from '../../services/card';
+import { Card } from '../../data/tarot-cards';
+import { CardItemComponent } from '../../components/card-item/card-item';
+import { FormsModule } from '@angular/forms'; // Necessário para o Two-Way Binding do input
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CardItemComponent, FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.scss'
 })
-export class Home {
+export class HomeComponent implements OnInit {
 
+  searchTerm: string = '';
+  allCards: Card[] = [];
+  filteredCards: Card[] = [];
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit(): void {
+    this.allCards = this.cardService.getAllCards();
+    this.filteredCards = this.allCards;
+  }
+
+  onSearch(): void {
+    this.filteredCards = this.cardService.getCardsByName(this.searchTerm);
+  }
 }
